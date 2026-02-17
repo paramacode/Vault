e9a33750a8fccf73905cb7315a5937541f7fcd10
paramacode <thealigatoryt@gmail.com>
2026-02-17 17:40:26 +0100

    0.1.0

--- docs/build/assets/js/aba21aa0.e55d51c4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkvault_docs=globalThis.webpackChunkvault_docs||[]).push([[361],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -21,7 +21,7 @@ const config = {
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
 	organizationName: "paramacode", // Usually your GitHub org/user name.
-	projectName: "Vault", // Usually your repo name.
+	projectName: "paramacode.github.io", // Usually your repo name.
 	deploymentBranch: "main", // <- publicar directamente en main
 
 	trailingSlash: false,
