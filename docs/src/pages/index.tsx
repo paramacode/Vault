cd930121b045dfb0eed10dd1fba5b4e0c0f3ca89
paramacode <thealigatoryt@gmail.com>
2026-02-17 15:52:50 +0100

    0.1.0

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -27,7 +27,7 @@ function HomepageHeader() {
 	);
 }
 
-export default function Home(): ReactNode {
+export default function Home() {
 	const { siteConfig } = useDocusaurusContext();
 	return (
 		<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
